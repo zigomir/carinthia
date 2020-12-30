afb1a1d1ab8846e8e452c7c99d62dc8d2c77b045
ziga <ziga@hey.com>
2020-12-30 16:55:30 +0100

    Form handling

--- examples/vue/src/main.js
+++ examples/vue/src/main.js
@@ -2,6 +2,19 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import './index.css'
 
+function appFactoryFunction() {
+  return createApp(App)
+}
+
+let app = appFactoryFunction()
+
 document.addEventListener('carinthia:load', () => {
-  createApp(App).mount('#app')
+  app.mount('#app')
+})
+
+document.addEventListener('carinthia:unload', () => {
+  app.unmount('#app')
+  // One can not re-mount same app instance
+  // https://github.com/vuejs/vue-next/issues/1287#issuecomment-638252620
+  app = appFactoryFunction()
 })
